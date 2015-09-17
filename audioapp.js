7cae2f6c2c64b09f0e9b9fe5341a0b485e78adaf
Victor Hugo <victor.h.alencar@gmail.com>
2015-09-17 08:20:57 -0300

    Download json files

--- audioapp.js
+++ audioapp.js
@@ -52,4 +52,27 @@ audioApp.controller("AudioController", ['$log', '$scope', 'ngAudio', '$timeout',
     control.inicio = 0;
     control.fim = 0;
   };
+
+  control.saveToPc = function (filename) {
+    var data = control.pieces;
+
+    if (!filename) {
+      filename = 'download.json';
+    }
+
+    if (typeof data === 'object') {
+      data = JSON.stringify(data, undefined, 2);
+    }
+
+    var blob = new Blob([data], {type: 'text/json'}),
+      e = document.createEvent('MouseEvents'),
+      a = document.createElement('a');
+
+    a.download = filename;
+    a.href = window.URL.createObjectURL(blob);
+    a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
+    e.initMouseEvent('click', true, false, window,
+        0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    a.dispatchEvent(e);
+  };
 }]);
